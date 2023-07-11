2e71f5beb0b761cafa993bf4f860918b86c6416a
Robert Tejada <robert_tejada@ucne.edu.do>
2023-07-10 20:31:06 -0400

    Agregando la migracion y la base de datos

---
+++ Server/Migrations/20230711002944_Inicial.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Parcial2Robert.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Entradas",
+                columns: table => new
+                {
+                    EntradaId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Concepto = table.Column<string>(type: "TEXT", nullable: true),
+                    PesoTotal = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CaantidadProducida = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entradas", x => x.EntradaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: true),
+                    PrecioCompra = table.Column<double>(type: "REAL", nullable: false),
+                    PrecioVenta = table.Column<double>(type: "REAL", nullable: false),
+                    Existencia = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.ProductoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EntradasDetalle",
+                columns: table => new
+                {
+                    DetalleId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EntradaId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CantidadUtilizada = table.Column<int>(type: "INTEGER", nullable: false),
+                    EntradasEntradaId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EntradasDetalle", x => x.DetalleId);
+                    table.ForeignKey(
+                        name: "FK_EntradasDetalle_Entradas_EntradasEntradaId",
+                        column: x => x.EntradasEntradaId,
+                        principalTable: "Entradas",
+                        principalColumn: "EntradaId");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "Descripcion", "Existencia", "PrecioCompra", "PrecioVenta" },
+                values: new object[,]
+                {
+                    { 1, "Mani", 250, 8.0, 15.0 },
+                    { 2, "Pistachos", 300, 15.0, 30.0 },
+                    { 3, "Pasas", 130, 10.0, 25.0 },
+                    { 4, "Ciruelas", 350, 25.0, 50.0 },
+                    { 5, "Mixto MPP", 320, 30.0, 60.0 },
+                    { 6, "Mixto MPC", 310, 30.0, 60.0 },
+                    { 7, "Mixto MPP", 250, 25.0, 50.0 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EntradasDetalle_EntradasEntradaId",
+                table: "EntradasDetalle",
+                column: "EntradasEntradaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EntradasDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+
+            migrationBuilder.DropTable(
+                name: "Entradas");
+        }
+    }
+}
